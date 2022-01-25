c5f3760cf8885aaa4cc79a75c58ecee429f11e64
ZhylaA <zak2010@i.ua>
2022-01-25 14:50:06 +0200

    task5

--- js/task-05.js
+++ js/task-05.js
@@ -9,6 +9,6 @@ const spanName = document.querySelector('span#name-output');
 input.addEventListener('input', onInputChange)
 function onInputChange(event) {
     console.log(event.currentTarget.value);
-    if (input.textContent.value==="") { spanName.textContent="Anonymous"; } else { spanName.textContent = event.currentTarget.value;};
+    if (input.value==="") { spanName.textContent="Anonymous"; } else { spanName.textContent = event.currentTarget.value;};
 };
 console.log(spanName.textContent)
